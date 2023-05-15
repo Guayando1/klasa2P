b1940f5eb0ac79390a536e7b6b7bb86f6026005a
uczen <amongmplay@gmail.com>
2023-05-15 12:21:26 +0200

    v1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(300, 300);
+            Console.Title = "Moja 1 apka";
+            Console.BackgroundColor= ConsoleColor.Green;
+            Console.ForegroundColor= ConsoleColor.Blue; 
+            Console.WriteLine("Klasa 2 P");
+            Console.WriteLine("Mechaniak");
+            Console.WriteLine(Console.CapsLock);
+            Console.Beep();
+            Console.Clear();
+            Console.ReadKey();
+            
+        }
+    }
+}
